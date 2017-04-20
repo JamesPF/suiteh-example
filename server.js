903b283ef0dce091bbfd271225f51b613b31cdc8
James Felz <james@still-water.com>
2017-04-20 11:10:18 -0400

    Configure for Heroku

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const hbs = require('express-handlebars');
 
 var app = express();
 
+const port = process.env.PORT || 3000;
+
 app.engine('handlebars', hbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '/public'));
@@ -15,6 +17,6 @@ app.get('/dashboard', (req, res) => {
   res.render('dashboard', {layout: 'dashboard'})
 });
 
-app.listen(3000, () => {
-  console.log('App is listening on port 3000');
+app.listen(port, () => {
+  console.log('App is listening on port ' + port);
 })
